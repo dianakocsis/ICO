b0b0e893ac8f0dfe5cdcae625f44cf34d7682796
Diana Kocsis <dlk61@pitt.edu>
2023-12-24 20:17:34 -0500

    adjust spacecoin to account for changes in erc20 standard

--- contracts/SpaceCoin.sol
+++ contracts/SpaceCoin.sol
@@ -1,13 +1,15 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ICO.sol";
 
 contract SpaceCoin is ERC20 {
 
     bool public tax;
-    address public owner;
-    address public treasury;
+    address public immutable owner;
+    address public immutable treasury;
+    address payable public immutable ico;
     uint constant maxSupply = 500000 * (10 ** 18);
     uint public supply;
     mapping(address => uint256) public balances;
@@ -17,32 +19,15 @@ contract SpaceCoin is ERC20 {
         _;
     }
 
-    /** @dev Sets the name, symbol, and owner and creates initial supply of tokens
-    */
-    constructor(uint _initialSupply, address _treasury) ERC20("SpaceCoin", "SPC") {
-        owner = msg.sender;
+    /// @notice Mint tokens to the ICO and treasury
+    /// @param _owner The owner of the contract
+    /// @param _treasury The address of the treasury
+    constructor(address _owner, address _treasury, address[] memory _allowList) ERC20("SpaceCoin", "SPC") {
+        owner = _owner;
+        ico = payable(address(new ICO(owner, this, _allowList)));
         treasury = _treasury;
-        _mint(msg.sender, _initialSupply);
-    }
-
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply.
-     *
-     * Requires that the amount of tokens will not exceed the maximum
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     */
-    function _mint(address account, uint amount) internal override {
-        require((supply + amount) <= maxSupply, "OVER_MAX");
-
-        _beforeTokenTransfer(address(0), account, amount);
-
-        supply += amount;
-        balances[account] += amount;
-        emit Transfer(address(0), account, amount);
-
-        _afterTokenTransfer(address(0), account, amount);
+        _mint(ico, 150_000 * 10 ** decimals());
+        _mint(treasury, 350_000 * 10 ** decimals());
     }
 
     /**
@@ -67,42 +52,9 @@ contract SpaceCoin is ERC20 {
     }
 
     /**
-     * @dev Moves `amount` of tokens from `sender` to `recipient`.
-     *      2% of the tokens in the transfer moves to the treasury address if tax is on
-     *
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` must have a balance of at least `amount`.
-     */
-    function _transfer(address sender, address recipient, uint256 amount) internal override {
-        
-        _beforeTokenTransfer(sender, recipient, amount);
-
-        uint256 senderBalance = balances[sender];
-        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
-        unchecked {
-            balances[sender] = senderBalance - amount;
-        }
-        
-        if (tax) {
-            uint256 oldAmt = amount;
-            amount = amount * 98 / 100;
-            balances[treasury] += oldAmt - amount;
-        } 
-        balances[recipient] += amount;
-
-        emit Transfer(sender, recipient, amount);
-
-        _afterTokenTransfer(sender, recipient, amount);
-    }
-
-    /**
      * @dev Owner sets the 2% tax on every transfer on or off
      */
     function setTax(bool control) external onlyOwner {
         tax = control;
-    } 
+    }
 }
